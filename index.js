ae8401bffb9a363a6b3138a1ef926ec7bf5beba7
Reid Chatham <reid.chatham@gmail.com>
2016-10-25 15:15:59 -0700

    changed pizza squad command

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ function navigateForInput(value) {
   if ((value === "home")||(value === "Home")) {
     window.location = "./index.html";
   }
-  if (value === "Pizza Squad") {
+  if (value === "PIZZA-NYE-2016-GL") {
     window.location = "./guest-list.html";
+  }
+  if (value.toLowerCase() === "pizza squad") {
+    var win = window.open("https://www.facebook.com/groups/dontstopthepizzaparty/", "_blank");
+    if (win) {
+      //Browser has allowed it to be opened
+      win.focus();
+    } else {
+      //Browser has blocked it
+      alert('Please allow popups for this website');
+    }
   }
   if (value.toLowerCase() === "find pizza") {
     var win = window.open("https://www.google.com/maps/search/pizza/", "_blank");
